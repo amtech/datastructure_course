feb45737eb39c4796fb0d55a1dff5ecf14d83b1b
nullmax <maxnull@hotmail.com>
2018-04-13 17:33:24 +0800

    Update and rename Main(huffman).java to Huffman.java

--- Huffman.java
+++ Huffman.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Main {
+public class Huffman {
     public static void main(String[] args) {
         HuffmanFrame huffmanFrame = new HuffmanFrame();
     }
